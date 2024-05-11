d4cc8380c642f854d8137407cfc236ea80b4bad9
juanrw <juan.rw24@gmail.com>
2024-05-11 18:18:20 -0500

    adjust recipe route and controller function to make protected API call

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -2,9 +2,11 @@ import { Recipe } from "../models/recipe.js"
 import axios from "axios"
 
 async function index (req, res) {
+  console.log(req.body.mealType)
   try {
-    axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}&cuisineType=${req.body.cuisineType}&random=true`)
+    axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}&mealType=${req.body.mealType}&random=true`)
     .then(response => {
+      console.log(response.data.hits)
       res.json(response.data.hits)
     })
   } catch (error) {
